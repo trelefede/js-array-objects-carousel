e85004053e9f74ff484ed5d58d960127b3e26a9e
TreleaniFederico <treleani.federico@gmail.com>
2022-09-07 16:28:01 +0200

    add array images add description on images add title on images

--- js/script.js
+++ js/script.js
@@ -13,7 +13,35 @@ dopo 5 secondi la slide avanza automaticamente
 const NUM_IMAGES = 5;
 const CHANGE_IMAGE_DELAY = 5;
 
-const images = createImageArray(NUM_IMAGES);
+const images = [
+    {
+        url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+        title: 'Svezia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Perù',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+        title: 'Chile',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Argentina',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+        title: 'Colombia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+];
 console.log(images);
 
 
@@ -35,44 +63,52 @@ rightArrowButton.addEventListener('click', moveCarouselForward);
 
 
 
-function moveCarouselForward(){
+function moveCarouselForward() {
     clearInterval(idInterval)
     // se l'indice si trova in fondo allora lo riposizione all'inizio dell'array
-    activeIndex = activeIndex < images.length -1 ? activeIndex +1 : 0 ;
+    activeIndex = activeIndex < images.length - 1 ? activeIndex + 1 : 0;
     buildCarousel(images, activeIndex);
     idInterval = setInterval(moveCarouselForward, CHANGE_IMAGE_DELAY * 1000);
 }
 
-function moveCarouselPrevious(){
-     clearInterval(idInterval)
+function moveCarouselPrevious() {
+    clearInterval(idInterval)
     // se l'indice è in prima posizione si valorizza all'ultima posizione dell'array
-    activeIndex = activeIndex > 0 ? activeIndex -1 : images.length -1 ;
+    activeIndex = activeIndex > 0 ? activeIndex - 1 : images.length - 1;
     buildCarousel(images, activeIndex);
     idInterval = setInterval(moveCarouselForward, CHANGE_IMAGE_DELAY * 1000);
 }
 
 
-function buildCarousel(urls, activeIndex){
+function buildCarousel(urls, activeIndex) {
     const carouselImages = document.querySelector('.carousel-images');
     const carouselThumbs = document.querySelector('.carousel-thumbs');
     let content = '';
-    for(let i = 0; i < urls.length; i++){
-        const url = urls[i];
+    let contentThumbs = '';
+    for (let i = 0; i < urls.length; i++) {
+        const url = urls[i].url;
+        const title = urls[i].title;
+        const description = urls[i].description;
+        const descriptionClass = i === activeIndex ? 'active' : 'none'
         const imageClass = i === activeIndex ? 'carousel-img active' : 'carousel-img'
-        content += `<img class="${imageClass}" src="${url}" alt="${url}" />`;
+        content += `<div class="card"> 
+                    <img class="${imageClass}" src="${url}" alt="${title}" />
+                    <span class="${descriptionClass}"><h2>${title}</h2>${description}</span>
+                    </div>`;
+        contentThumbs += `<img class="${imageClass}" src="${url}" alt="${title}"/>`
     }
     // console.log({content});
     carouselImages.innerHTML = content;
-    carouselThumbs.innerHTML = content;
+    carouselThumbs.innerHTML = contentThumbs;
 }
 
 
-function createImageArray(numImages){
+function createImageArray(numImages) {
     const images = [];
-    for(let i = 1; i <= numImages; i++){
+    for (let i = 1; i <= numImages; i++) {
         const fileName = i < 10 ? '0' + i : i;
         const url = 'img/' + fileName + '.jpg';
-        images.push(url); 
+        images.push(url);
     }
 
     return images;
